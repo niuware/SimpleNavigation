e9927addbf037fbf13034bb86fb3420a81d33b36
Erik Lopez <maileks@gmail.com>
2022-01-17 16:55:27 +0900

    Create Feed entity

---
+++ SimpleNavigation/Entities/Feed.swift
@@ -0,0 +1,13 @@
+//
+//  Feed.swift
+//  SimpleNavigation
+//
+//  Created by Erik Lopez on 2022/01/11.
+//
+
+import Foundation
+
+enum Feed: String {
+    case home = "Feed"
+    case music = "Music"
+}
